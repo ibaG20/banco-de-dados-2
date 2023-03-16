47ef6ab6be0b6756b9a637cc4d95044743f31a26
ibaG20 <marretogabriella@gmail.com>
2023-03-15 22:20:50 -0300

    scripts de insert

--- bd1-revisão.sql
+++ bd1-revisão.sql
@@ -5,28 +5,28 @@ CREATE TABLE Equipe (
 
 CREATE SEQUENCE seq_equipe START WITH 1 INCREMENT BY 1;
 
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Palmeiras');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Santos');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'SCCP');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'SPFC');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Flamengo');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Fluminense');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Vasco');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Botafogo');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Gremio');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Internacional');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Athletico PR');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Coritiba');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Atletico MG');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Cruzeiro');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Bahia');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Vitoria');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Sport');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Nautico');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Ponte Preta');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Guarani');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Portuguesa');
-INSERT INTO equipe (id, nome) VALUES (seq_equipe.NEXTVAL, 'Chapecoense');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Palmeiras');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Santos');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'SCCP');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'SPFC');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Flamengo');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Fluminense');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Vasco');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Botafogo');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Gremio');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Internacional');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Athletico PR');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Coritiba');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Atletico MG');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Cruzeiro');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Bahia');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Vitoria');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Sport');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Nautico');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Ponte Preta');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Guarani');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Portuguesa');
+INSERT INTO equipe (id, nome) VALUES (NEXTVAL('seq_equipe'), 'Chapecoense');
 
 
 CREATE TABLE campeonato (
@@ -36,8 +36,8 @@ CREATE TABLE campeonato (
 
 CREATE SEQUENCE seq_campeonato START WITH 1 INCREMENT BY 1;
 
-INSERT INTO campeonato (id, nome) VALUES (seq_campeonato.NEXTVAL, 'Campeonato Brasileiro');
-INSERT INTO campeonato (id, nome) VALUES (seq_campeonato.NEXTVAL, 'Copa do Brasil');
+INSERT INTO campeonato (id, nome) VALUES (NEXTVAL('seq_campeonato'), 'Campeonato Brasileiro');
+INSERT INTO campeonato (id, nome) VALUES (NEXTVAL('seq_campeonato'), 'Copa do Brasil');
 
 CREATE TABLE jogador_brasileiro (
   id INTEGER,
@@ -58,21 +58,21 @@ CREATE TABLE jogador_brasileiro (
 CREATE SEQUENCE seq_jogador START WITH 1 INCREMENT BY 1;
 
 INSERT INTO jogador_brasileiro (id, cpf, nome, posicao, id_equipe, salario) 
-  VALUES (seq_jogador.NEXTVAL, '111.222.333.44', 'Sao Marcos', 'Goleiro', 1, 350000.75);
+  VALUES (NEXTVAL('seq_jogador'), '111.222.333.44', 'Sao Marcos', 'Goleiro', 1, 350000.75);
 INSERT INTO jogador_brasileiro (id, cpf, nome, posicao, id_equipe, salario) 
-  VALUES (seq_jogador.NEXTVAL, '098.765.345.12', 'Neymar', 'Atacante', 2, 565000.50);
+  VALUES (NEXTVAL('seq_jogador'), '098.765.345.12', 'Neymar', 'Atacante', 2, 565000.50);
 INSERT INTO jogador_brasileiro (id, cpf, nome, posicao, id_equipe, salario)  
-  VALUES (seq_jogador.NEXTVAL, '045.456.555.12', 'Betao', 'Zagueiro', 3, 18000.00);
+  VALUES (NEXTVAL('seq_jogador'), '045.456.555.12', 'Betao', 'Zagueiro', 3, 18000.00);
 INSERT INTO jogador_brasileiro (id, cpf, nome, posicao, id_equipe, salario) 
-  VALUES (seq_jogador.NEXTVAL, '235.457.789-65', 'Alex', 'Meio-Campo', 1, 310000.20);
+  VALUES (NEXTVAL('seq_jogador'), '235.457.789-65', 'Alex', 'Meio-Campo', 1, 310000.20);
 INSERT INTO jogador_brasileiro (id, cpf, nome, posicao, id_equipe, salario)  
-  VALUES (seq_jogador.NEXTVAL, '777.854.123-68', 'Marinho', 'Atacante', 2, 425000.00);
+  VALUES (NEXTVAL('seq_jogador'), '777.854.123-68', 'Marinho', 'Atacante', 2, 425000.00);
 INSERT INTO jogador_brasileiro (id, cpf, nome, posicao, id_equipe, salario)  
-  VALUES (seq_jogador.NEXTVAL, '159.487.236-00', 'Gilmar Fuba', 'Atacante', 3, 8500.00);
+  VALUES (NEXTVAL('seq_jogador'), '159.487.236-00', 'Gilmar Fuba', 'Atacante', 3, 8500.00);
 INSERT INTO jogador_brasileiro (id, cpf, nome, posicao, id_equipe, salario)  
-  VALUES (seq_jogador.NEXTVAL, '457.148.230-00', 'Iranildo', 'Meio-Campo', null, 0);
+  VALUES (NEXTVAL('seq_jogador'), '457.148.230-00', 'Iranildo', 'Meio-Campo', null, 0);
 INSERT INTO jogador_brasileiro (id, cpf, nome, posicao, id_equipe, salario)  
-  VALUES (seq_jogador.NEXTVAL, '476.555.788-15', 'Odvan', 'Zagueiro', null, 0);
+  VALUES (NEXTVAL('seq_jogador'), '476.555.788-15', 'Odvan', 'Zagueiro', null, 0);
 
 CREATE TABLE jogador_estrangeiro (
   id INTEGER,
@@ -89,11 +89,11 @@ CREATE TABLE jogador_estrangeiro (
 );
 
 INSERT INTO jogador_estrangeiro (id, passaporte, nome, posicao, id_equipe, pais_origem, salario) 
-  VALUES (seq_jogador.NEXTVAL, '586.324.111.44', 'Arce', 'Lateral', 1, 'Paraguai', 200500.10);
+  VALUES (NEXTVAL('seq_jogador'), '586.324.111.44', 'Arce', 'Lateral', 1, 'Paraguai', 200500.10);
 INSERT INTO jogador_estrangeiro (id, passaporte, nome, posicao, id_equipe, pais_origem, salario) 
-  VALUES (seq_jogador.NEXTVAL, '348.562.363.14', 'Mancuso', 'Atacante', 3, 'Argentina', 110000.75);
+  VALUES (NEXTVAL('seq_jogador'), '348.562.363.14', 'Mancuso', 'Atacante', 3, 'Argentina', 110000.75);
 INSERT INTO jogador_estrangeiro (id, passaporte, nome, posicao, id_equipe, pais_origem, salario) 
-  VALUES (seq_jogador.NEXTVAL, '456.357.698.47', 'Gioino', 'Atacante', 3, 'Argentina', 15000);
+  VALUES (NEXTVAL('seq_jogador'), '456.357.698.47', 'Gioino', 'Atacante', 3, 'Argentina', 15000);
 
 CREATE TABLE equipe_campeonato (
   id_equipe INTEGER,
@@ -125,11 +125,11 @@ CREATE TABLE cidade (
 
 CREATE SEQUENCE seq_cidade START WITH 1 INCREMENT BY 1;
 
-INSERT INTO cidade (id, nome) VALUES (seq_cidade.NEXTVAL, 'Sao Paulo');
-INSERT INTO cidade (id, nome) VALUES (seq_cidade.NEXTVAL, 'Rio de Janeiro');
-INSERT INTO cidade (id, nome) VALUES (seq_cidade.NEXTVAL, 'Belo Horizonte');
-INSERT INTO cidade (id, nome) VALUES (seq_cidade.NEXTVAL, 'Porto Alegre');
-INSERT INTO cidade (id, nome) VALUES (seq_cidade.NEXTVAL, 'Recife');
+INSERT INTO cidade (id, nome) VALUES (NEXTVAL('seq_cidade'), 'Sao Paulo');
+INSERT INTO cidade (id, nome) VALUES (NEXTVAL('seq_cidade'), 'Rio de Janeiro');
+INSERT INTO cidade (id, nome) VALUES (NEXTVAL('seq_cidade'), 'Belo Horizonte');
+INSERT INTO cidade (id, nome) VALUES (NEXTVAL('seq_cidade'), 'Porto Alegre');
+INSERT INTO cidade (id, nome) VALUES (NEXTVAL('seq_cidade'), 'Recife');
 
 CREATE TABLE tecnico (
   id INTEGER,
@@ -146,17 +146,17 @@ CREATE TABLE tecnico (
 CREATE SEQUENCE seq_tecnico START WITH 1 INCREMENT BY 1;
 
 INSERT INTO Tecnico (id, cpf, nome, id_equipe, data_nasc) 
-  VALUES (seq_tecnico.NEXTVAL, '123.654.875-90', 'Osvaldo Brandao', 1, TO_DATE('18-09-1916', 'dd-mm-yyyy'));
+  VALUES (NEXTVAL('seq_tecnico'), '123.654.875-90', 'Osvaldo Brandao', 1, TO_DATE('18-09-1916', 'dd-mm-yyyy'));
 INSERT INTO Tecnico (id, cpf, nome, id_equipe, data_nasc) 
-  VALUES (seq_tecnico.NEXTVAL, '565.187.765-12', 'Vicente Feola', 2, TO_DATE('01/11/1909', 'dd/mm/yyyy'));
+  VALUES (NEXTVAL('seq_tecnico'), '565.187.765-12', 'Vicente Feola', 2, TO_DATE('01/11/1909', 'dd/mm/yyyy'));
 INSERT INTO Tecnico (id, cpf, nome, id_equipe, data_nasc)  
-  VALUES (seq_tecnico.NEXTVAL, '098.904.642-65', 'Tite', 3, TO_DATE('25/05/1961', 'dd/mm/yyyy'));
+  VALUES (NEXTVAL('seq_tecnico'), '098.904.642-65', 'Tite', 3, TO_DATE('25/05/1961', 'dd/mm/yyyy'));
 INSERT INTO Tecnico (id, cpf, nome, id_equipe, data_nasc) 
-  VALUES (seq_tecnico.NEXTVAL, '187.644.235-20', 'Tele Santana', 4, TO_DATE('26/07/1931', 'dd/mm/yyyy'));
+  VALUES (NEXTVAL('seq_tecnico'), '187.644.235-20', 'Tele Santana', 4, TO_DATE('26/07/1931', 'dd/mm/yyyy'));
 INSERT INTO Tecnico (id, cpf, nome, id_equipe, data_nasc) 
-  VALUES (seq_tecnico.NEXTVAL, '145.687.789-14', 'Joel Santana', null, TO_DATE('25/12/1948', 'dd/mm/yyyy'));
+  VALUES (NEXTVAL('seq_tecnico'), '145.687.789-14', 'Joel Santana', null, TO_DATE('25/12/1948', 'dd/mm/yyyy'));
 INSERT INTO Tecnico (id, cpf, nome, id_equipe, data_nasc)  
-  VALUES (seq_tecnico.NEXTVAL, '546.654.456-08', 'Emerson Leao', null, TO_DATE('11/07/1949', 'dd/mm/yyyy'));
+  VALUES (NEXTVAL('seq_tecnico'), '546.654.456-08', 'Emerson Leao', null, TO_DATE('11/07/1949', 'dd/mm/yyyy'));
     
 CREATE TABLE paises_tecnicos (
   id_tecnico INTEGER,
@@ -205,31 +205,31 @@ CREATE SEQUENCE seq_jogo START WITH 1 INCREMENT BY 1;
 
 INSERT INTO jogo (id, data_jogo, id_equipe_casa, id_equipe_fora, 
                 gols_equipe_casa, gols_equipe_fora, id_cidade, estadio, id_campeonato)
-  VALUES (seq_jogo.NEXTVAL, TO_DATE('10/06/1994', 'dd/mm/yyyy'), 3, 1, 0, 2, 1, 'Itaquerao', 1);
+  VALUES (NEXTVAL('seq_jogo'), TO_DATE('10/06/1994', 'dd/mm/yyyy'), 3, 1, 0, 2, 1, 'Itaquerao', 1);
 INSERT INTO jogo (id, data_jogo, id_equipe_casa, id_equipe_fora, 
                 gols_equipe_casa, gols_equipe_fora, id_cidade, estadio, id_campeonato)
-  VALUES (seq_jogo.NEXTVAL, TO_DATE('17/06/1994', 'dd/mm/yyyy'), 2, 4, 2, 2, 1, 'Vila Belmiro', 1);
+  VALUES (NEXTVAL('seq_jogo'), TO_DATE('17/06/1994', 'dd/mm/yyyy'), 2, 4, 2, 2, 1, 'Vila Belmiro', 1);
 INSERT INTO jogo (id, data_jogo, id_equipe_casa, id_equipe_fora, 
                 gols_equipe_casa, gols_equipe_fora, id_cidade, estadio, id_campeonato)
-  VALUES (seq_jogo.NEXTVAL, TO_DATE('15/06/1994', 'dd/mm/yyyy'), 2, 3, 3, 2, 1, 'Morumbi', 1);
+  VALUES (NEXTVAL('seq_jogo'), TO_DATE('15/06/1994', 'dd/mm/yyyy'), 2, 3, 3, 2, 1, 'Morumbi', 1);
 INSERT INTO jogo (id, data_jogo, id_equipe_casa, id_equipe_fora, 
                 gols_equipe_casa, gols_equipe_fora, id_cidade, estadio, id_campeonato)
-  VALUES (seq_jogo.NEXTVAL, TO_DATE('10/07/1994', 'dd/mm/yyyy'), 5, 8, 2, 1, 2, 'Maracana', 1);
+  VALUES (NEXTVAL('seq_jogo'), TO_DATE('10/07/1994', 'dd/mm/yyyy'), 5, 8, 2, 1, 2, 'Maracana', 1);
 INSERT INTO jogo (id, data_jogo, id_equipe_casa, id_equipe_fora, 
                 gols_equipe_casa, gols_equipe_fora, id_cidade, estadio, id_campeonato)
-  VALUES (seq_jogo.NEXTVAL, TO_DATE('11/06/1994', 'dd/mm/yyyy'), 6, 7, 4, 3, 3, 'Mineirao', 1);
+  VALUES (NEXTVAL('seq_jogo'), TO_DATE('11/06/1994', 'dd/mm/yyyy'), 6, 7, 4, 3, 3, 'Mineirao', 1);
 INSERT INTO jogo (id, data_jogo, id_equipe_casa, id_equipe_fora, 
                 gols_equipe_casa, gols_equipe_fora, id_cidade, estadio, id_campeonato)
-  VALUES (seq_jogo.NEXTVAL, TO_DATE('25/07/1994', 'dd/mm/yyyy'), 9, 10, 4, 0, 4, 'Beira Rio', 1);
+  VALUES (NEXTVAL('seq_jogo'), TO_DATE('25/07/1994', 'dd/mm/yyyy'), 9, 10, 4, 0, 4, 'Beira Rio', 1);
 INSERT INTO jogo (id, data_jogo, id_equipe_casa, id_equipe_fora, 
                 gols_equipe_casa, gols_equipe_fora, id_cidade, estadio, id_campeonato)
-  VALUES (seq_jogo.NEXTVAL, TO_DATE('20/07/1994', 'dd/mm/yyyy'), 5, 3, 2, 1, 2, 'Maracana', 1);
+  VALUES (NEXTVAL('seq_jogo'), TO_DATE('20/07/1994', 'dd/mm/yyyy'), 5, 3, 2, 1, 2, 'Maracana', 1);
 INSERT INTO jogo (id, data_jogo, id_equipe_casa, id_equipe_fora, 
                 gols_equipe_casa, gols_equipe_fora, id_cidade, estadio, id_campeonato)
-  VALUES (seq_jogo.NEXTVAL, TO_DATE('20/07/1994', 'dd/mm/yyyy'), 5, 7, 3, 1, 4, 'Beira Rio', 1);
+  VALUES (NEXTVAL('seq_jogo'), TO_DATE('20/07/1994', 'dd/mm/yyyy'), 5, 7, 3, 1, 4, 'Beira Rio', 1);
 INSERT INTO jogo (id, data_jogo, id_equipe_casa, id_equipe_fora, 
                 gols_equipe_casa, gols_equipe_fora, id_cidade, estadio, id_campeonato)
-  VALUES (seq_jogo.NEXTVAL, TO_DATE('20/07/1994', 'dd/mm/yyyy'), 18, 20, 0, 2, 3, 'Mineirao', 1);
+  VALUES (NEXTVAL('seq_jogo'), TO_DATE('20/07/1994', 'dd/mm/yyyy'), 18, 20, 0, 2, 3, 'Mineirao', 1);
 INSERT INTO jogo (id, data_jogo, id_equipe_casa, id_equipe_fora, 
                 gols_equipe_casa, gols_equipe_fora, id_cidade, estadio, id_campeonato)
-  VALUES (seq_jogo.NEXTVAL, TO_DATE('29/07/1994', 'dd/mm/yyyy'), 6, 12, 2, 1, null, null, 1);
+  VALUES (NEXTVAL('seq_jogo'), TO_DATE('29/07/1994', 'dd/mm/yyyy'), 6, 12, 2, 1, null, null, 1);
